19615ecac90e102f1ce4fb673c1f10f8d00fea19
bensalcie <bensalcie@gmail.com>
2020-10-04 11:55:08 +0300

    Initial commit

---
+++ Mpesa Android/src/main/java/bensalcie/payhero/mpesa/mpesa/services/STKPushService.kt
@@ -0,0 +1,17 @@
+package bensalcie.payhero.mpesa.mpesa.services
+
+import bensalcie.payhero.mpesa.mpesa.model.AccessToken
+import bensalcie.payhero.mpesa.mpesa.model.STKPush
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+
+interface STKPushService {
+    @POST("mpesa/stkpush/v1/processrequest")
+    fun sendPush(@Body stkPush: STKPush): Call<STKPush>
+
+    @GET("oauth/v1/generate?grant_type=client_credentials")
+    fun getAccessToken(): Call<AccessToken>
+}
